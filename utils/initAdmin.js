787647502bd9d453fb0bc71cdf12aade89e029ff
John Manuvel <sjohnmanuel04@gmail.com>
2025-05-31 14:37:03 +0530

    31

--- utils/initAdmin.js
+++ utils/initAdmin.js
@@ -2,9 +2,9 @@ const Admin = require('../models/Admin');
 const bcrypt = require('bcryptjs');
 
 const initAdmin = async () => {
-  const admin = await Admin.findOne({ username: 'admin' });
+  const admin = await Admin.findOne({ username: 'john' });
   if (!admin) {
-    const hashed = await bcrypt.hash('admin123', 10);
+    const hashed = await bcrypt.hash('john01', 10);
     await new Admin({ username: 'admin', email: 'sjohnmanuelpc@gmail.com', password: hashed }).save();
     console.log('Default admin created');
   }
